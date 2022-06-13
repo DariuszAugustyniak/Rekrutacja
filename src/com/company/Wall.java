33452ebb88d7e7595f8491181fd247d133d470d2
DariuszAugustyniak <41304670+DariuszAugustyniak@users.noreply.github.com>
2022-06-13 12:53:11 +0200

    Poprawki po przeczytaniu

--- src/com/company/Wall.java
+++ src/com/company/Wall.java
@@ -21,7 +21,7 @@ public class Wall implements Structure {
 
 
     public Optional findBlockByColor(String color) {
-        for (Block block:blocks) {
+        for (Block block : blocks) {
 
             if (block.getColor().equals(color)) {
                 return Optional.ofNullable(block);
@@ -33,8 +33,8 @@ public class Wall implements Structure {
 
 
     public List findBlocksByMaterial(String material) {
-        ArrayList<Block> blocksByMaterial = new ArrayList<>();
-        for (Block block:blocks) {
+        ArrayList<Block> blocksByMaterial = new ArrayList<Block>();
+        for (Block block : blocks) {
             if (block.getMaterial().equals(material)) {
                 blocksByMaterial.add(block);
             }
